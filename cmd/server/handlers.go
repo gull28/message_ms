952a09aa3839b3461ed4d2052954f40815b02cdd
gull28 <krisjanisaugusts@gmail.com>
2024-05-14 20:56:38 +0300

    Code reformat

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -9,3 +9,12 @@ func getMessages(r *http.Request, w http.ResponseWriter) {
 
 	w.Header().Set("Content-Type", "application/json")
 }
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if validateMethod(r, w, http.MethodGet) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "Hello, World!"}`))
+
+		return
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bye, World!")
 }
 
 func main() {
+	port := flag.String("port", ":8080", "HTTP Port to run the server on")
 	http.HandleFunc("/hello", handler)
+	flag.Parse()
+
+	// simple logger
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *port,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
 	// http.HandleFunc("/messages", getMessages)
 
-	fmt.Println("Server is running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	infoLog.Printf("Starting server on %s", *port)
+	err := srv.ListenAndServe()
+
+	errorLog.Fatal(err)
+
 }
