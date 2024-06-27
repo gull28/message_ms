cf10c2bdbbfabfb0aa088abe89d6a0b95cbe7d9e
gull28 <krisjanisaugusts@gmail.com>
2024-06-27 15:50:52 +0300

    route updates

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -8,7 +8,9 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.Handle("/", MultipleMiddleware(http.HandlerFunc(app.home), AuthMiddleware, MethodMiddleware(http.MethodGet)))
 
-	mux.Handle("/send-code", MultipleMiddleware(http.HandlerFunc(app.home), AuthMiddleware, MethodMiddleware(http.MethodPost)))
+	mux.Handle("/send-code", MultipleMiddleware(http.HandlerFunc(app.sendCode), AuthMiddleware, MethodMiddleware(http.MethodPost)))
+
+	mux.Handle("/verify-code", MultipleMiddleware(http.HandlerFunc(app.validateCode), AuthMiddleware, MethodMiddleware(http.MethodPost)))
 
 	return mux
 }
