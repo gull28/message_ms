18c7674a45626c1a8fe335302f213d1621f3984b
gull28 <krisjanisaugusts@gmail.com>
2024-06-27 16:21:19 +0300

    Readme update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-[!!In development!!]
+# Microservice for user validation codes
 
-A messaging microservice.
+## Running the service
 
-Docs
+- Clone the repository
+- Run `go get` for installing go packages
+
+### Caveats
+If you wish to use email messaging only, the service is ready to go. Now, if you also want SMS handling, you will have to add a little bit of your own code based on the SMS provider you use. 
+In most cases, this is very simple and all the information is provided by each specific SMS API provider. Simply edit the `sms.go` file with the specific code from the provider.
+
+If you want to make a custom email with your own variables, use the `/delivery/templates` folder to store your html template to use in your email
+
+
 
--- internal/delivery/email.go
+++ internal/delivery/email.go
@@ -1,32 +1,36 @@
 package delivery
 
 import (
+	"fmt"
 	"net/smtp"
+	"os"
+	"strings"
 
 	"github.com/gull28/message_ms/internal/config"
 )
 
+// SendMail sends an email with an HTML body loaded from a template file
 func SendMail(to string, subject string, code string) error {
+	// Load SMTP configuration
 	smtpConfig := config.LoadConfig().Smtp
-
 	smtpHost := smtpConfig.Host
 	smtpPort := smtpConfig.Port
 	smtpUser := smtpConfig.Username
 	smtpPassword := smtpConfig.Password
 
+	htmlBody, err := LoadHTMLTemplate("./templates/index.html", code)
+	if err != nil {
+		return err
+	}
+
 	auth := smtp.PlainAuth("", smtpUser, smtpPassword, smtpHost)
 
-	body := BuildEmailMessage(code)
-	// body := "Your verification code is: " + code
 	from := smtpUser
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: " + subject + "\n\n" +
-		body
+	msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\nMIME-Version: 1.0\nContent-Type: text/html; charset=\"UTF-8\"\n\n%s",
+		from, to, subject, htmlBody)
 
 	recipient := []string{to}
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, recipient, []byte(msg))
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, recipient, []byte(msg))
 	if err != nil {
 		return err
 	}
@@ -34,8 +38,15 @@ func SendMail(to string, subject string, code string) error {
 	return nil
 }
 
-func BuildEmailMessage(code string) string {
-	// implement your own email message for production
-	// use a template engine or a library to build the message for convenience
-	return "Your verification code is: " + code
+func LoadHTMLTemplate(filePath string, code string) (string, error) {
+	htmlData, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	htmlContent := string(htmlData)
+
+	htmlContent = strings.Replace(htmlContent, "{{.Code}}", code, -1)
+
+	return htmlContent, nil
 }
